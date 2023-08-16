6492d04544faf9d25beb5fb79916098762b53561
codingjwp <codingjwp@gmail.com>
2023-08-16 11:46:46 +0900

    fix: json에 이미지 이름 내용 추가

--- crolring/csvofjson.py
+++ crolring/csvofjson.py
@@ -1,7 +1,7 @@
 import csv, json
-
+# CSV OF JSON
 input_file = './pokemon.csv'
-out_file = './pokemon.json'
+out_file = './new_pokemon.json'
 
 with open(input_file, 'r', encoding='utf-8') as csv_file:
   csv_reader = csv.reader(csv_file)
@@ -10,10 +10,30 @@ with open(input_file, 'r', encoding='utf-8') as csv_file:
 
   lines = []
   for row in csv_reader:
+    print(row)
     row_data = {header[i]: row[i] for i in range(len(header))}
     lines.append(row_data)
 
 with open(out_file, 'w', encoding='utf-8') as json_file:
-  json.dump(lines, json_file, ensure_ascii=False)
+  json.dump(lines, json_file, ensure_ascii=False, indent=2)
 
-  # , indent=2
+
+# JSON OF CSV
+# input_file = './pokemon.json'
+# out_file = './new_pokemon.csv'
+
+# with open(input_file, 'r', encoding='utf-8') as json_file:
+#     data = json.load(json_file)
+
+# header = data[0].keys()
+
+# with open(out_file, 'w', encoding='utf-8') as csv_file:
+#   csv_writer = csv.writer(csv_file)
+
+#   csv_writer.writerow(header)
+
+#   for row in data:
+#     updated_row = list(row.values())
+#     updated_row[2] = updated_row[2].replace('-', ' ')
+#     updated_row[3] = updated_row[3].replace('-', ' ')
+#     csv_writer.writerow(updated_row)
