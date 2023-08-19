7826fa7708c9463db47c73c8a4776fb4d5eda528
codingjwp <codingjwp@gmail.com>
2023-08-19 17:13:54 +0900

    style: 검색바 스타일 수정

--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -1,27 +1,29 @@
-import { ChangeEvent, FormEvent, FocusEvent, forwardRef } from 'react';
+import { MouseEvent, forwardRef } from 'react';
 import styles from './searchBar.module.scss';
 
 interface SearchBarProps {
   name: string;
-  onChnage?: (e: ChangeEvent) => void;
-  onFocus?: (e: FocusEvent) => void;
-  onBlur?: (e: FocusEvent) => void;
-  onInvalid?: (e: FormEvent) => void;
 }
 
-const SearchBar = forwardRef<HTMLInputElement, SearchBarProps>(({name, onChnage, onFocus, onBlur, onInvalid}, ref) => {
+const SearchBar = forwardRef<HTMLInputElement, SearchBarProps>(({name}, ref) => {
 
-  return (
-    <form className={styles.searchBar} onSubmit={(e)=>{e?.preventDefault()}}>
-      <label className={styles.searchLabel} htmlFor={name} autoFocus={false}>Search</label>
-      <input className={styles.searchInput} ref={ref} id={name} type='search' name={name} pattern='.*\S.*' required onChange={onChnage} onFocus={onFocus} onBlur={onBlur} onInvalid={onInvalid} />
-      <button className={styles.searchBtn} type="submit" title={name} name={name}>
-        <span className={styles.searchBtnSpan}>Search</span>
-      </button>
-    </form>
-  )
+  const emptyValueForcus = (e: MouseEvent) => {
+    console.log("testsssssssssss");
+  }
+
+return (
+  <div className={styles.searchBar}>
+    <label className={styles.searchLabel} htmlFor={name}>Search</label>
+    <input className={styles.searchInput} ref={ref} id={name} type='search' name={name} pattern='.*\S.*' required={false} />
+    <button className={styles.searchBtn} type="button" onClick={emptyValueForcus} title={name} name={name}>
+      <span className={styles.searchBtnSpan}>Search</span>
+    </button>
+  </div>
+)
+
 
 });
 export default SearchBar;
+
 
 
