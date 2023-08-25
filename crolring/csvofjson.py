95084db747142dbda5843d3fcc5913e7868ea60a
codingjwp <codingjwp@gmail.com>
2023-08-25 15:02:20 +0900

    feat: pokemondb, list csv, json 수정

--- crolring/csvofjson.py
+++ crolring/csvofjson.py
@@ -1,7 +1,7 @@
-import csv, json
+import csv, json, re
 # CSV OF JSON
-input_file = './pokemonlist.csv'
-out_file = './pokemonlist.json'
+input_file = './pokemondb.csv'
+out_file = './pokemondb.json'
 
 with open(input_file, 'r', encoding='utf-8') as csv_file:
   csv_reader = csv.reader(csv_file)
@@ -35,3 +35,19 @@ with open(out_file, 'w', encoding='utf-8') as json_file:
 #     updated_row[2] = updated_row[2].replace('-', ' ')
 #     updated_row[3] = updated_row[3].replace('-', ' ')
 #     csv_writer.writerow(updated_row)
+
+# CSV of CSV
+# input_file = './pokemondb.csv'
+# out_file = './pokemonlist.csv'
+
+# with open(input_file, 'r', encoding='utf-8') as csv_file:
+#   csv_reader = csv.reader(csv_file)
+#   lint = []
+#   for row in csv_reader:
+#     data = [row[0], row[2], re.sub(r"'|\+|_\s", '', row[2]).lower(), row[16]]
+#     lint.append(data)
+
+# with open(out_file, 'w', encoding='utf-8') as new_file:
+#   csv_writer = csv.writer(new_file);
+#   for row in lint:
+#     csv_writer.writerow(row)
